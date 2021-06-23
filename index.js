f60b24fa1e29ea3d40f5787f6832f1afe27ab6f8
Mohammed Zakaria Alalaya <mohammed.alalaya@gmail.com>
2021-06-23 02:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 class Breakfast{
+<<<<<<< HEAD
     constructor(food, drink) {
     this.food = food;
     this.drink = drink;
+=======
+    constructor(food1, food2) {
+    this.food1 = food1;
+    this.food2 = food2;
+>>>>>>> 75fd53d76e6e483b5cf54d92add1b4c824f73ebc
   }
 }
 
  class Lunch{
+<<<<<<< HEAD
     constructor(salad, soup, drink) {
     this.salad = salad;
     this.drink = drink;
@@ -26,4 +33,15 @@ class Breakfast{
  const bfast = new Breakfast('eggs', 'juice')
  const  newlunch = new Lunch('side salad', 'broccoli cheddar soup', 'iced tea')
  const newdinner = new Dinner('balsamic salad', 'consomme', 'filet mignon', 'cheesecake')
+=======
+    constructor(food1, food2) {
+    this.food1 = food1;
+    this.food2 = food2;
+  }
+}
+ 
+ 
+ const bfast = new Breakfast('eggs', 'juice')
+ 
+>>>>>>> 75fd53d76e6e483b5cf54d92add1b4c824f73ebc
  
